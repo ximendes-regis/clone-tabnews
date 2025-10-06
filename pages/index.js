a876d3a95648fdfc454fa20cff75d1cae145015a
Regis Ximendes <regisximendes.dev@gmail.com>
2025-10-06 11:55:52 -0300

    fix project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-function Home() {
-  return <h1>Tab News</h1>;
+import React from "react";
+import ReactDOM from "react-dom";
+
+function UnderConstruction() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
+      <h1 className="text-4xl font-bold text-gray-800">Site em Construção</h1>
+      <p className="text-lg text-gray-600 mt-4">
+        Estamos trabalhando para trazer algo incrível em breve!
+      </p>
+      <div className="mt-6">
+        <span className="inline-block w-12 h-12 border-4 border-blue-500 border-dashed rounded-full animate-spin"></span>
+      </div>
+    </div>
+  );
 }
 
-export default Home;
+// ReactDOM.render(<UnderConstruction />, document.getElementById("root"));
+export default UnderConstruction;
