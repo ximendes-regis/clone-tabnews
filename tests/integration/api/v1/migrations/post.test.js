f713f5b288d1230ac70ad9e959f9b5c35d295b44
Regis Ximendes <regisximendes.dev@gmail.com>
2025-10-14 19:22:15 -0300

    make npm test more rebust

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,10 @@
 import db from "infra/database.js";
+import orchestrador from "tests/orchestrador.js";
 
-beforeAll(clearDatabase);
-
-async function clearDatabase() {
+beforeAll(async () => {
+  await orchestrador.waitForAllServices();
   await db.query("drop schema public cascade; create schema public");
-}
+});
 
 test("POST to api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
